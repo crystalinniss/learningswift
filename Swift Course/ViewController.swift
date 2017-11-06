073224192824c289eddf022a06487be537799c96
Crystal Inniss <crystal@Crystals-MacBook-Pro.local>
2017-11-06 14:21:30 -0800

    Our first commit on this project

--- Swift Course/ViewController.swift
+++ Swift Course/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    var buttonCount = 0
+    
+    @IBOutlet weak var mycapitallabel: UILabel!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount = buttonCount + 1
+        
+        print (buttonCount)
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.blue
+            mycapitallabel.text = "Wow, 10 times, that's amazing!"
+        }
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.red
+        
+        mycapitallabel.text = "Crystal Is Awesome!"
     }
 
     override func didReceiveMemoryWarning() {
